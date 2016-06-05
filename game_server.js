8d49dd2117a1cb892e0bc283f43ae7defadc8d0e
yh157612 <b02902039@ntu.edu.tw>
2016-06-05 23:06:12 +0800

    小整理

--- game_server.js
+++ game_server.js
@@ -1,11 +1,8 @@
-module.exports = function (httpserver) {
-	var io = require('socket.io')(httpserver);
+module.exports = function (io) {
 	
 	io.on('connection', function (socket) {
 		socket.on('position', function (data) {
-			console.log(socket.id + ' pos: ' + data.x + ' ' + data.y);
 			data.id = socket.id;
-			// io.emit('position', data);
 			socket.broadcast.emit('position', data);
 		});
 		socket.on('hello', function (data) {
